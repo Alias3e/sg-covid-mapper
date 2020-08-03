65a2541aaf2556f80905d2946a4d3c983e7f130a
hsaochai <hsaochai@gmail.com>
2020-08-03 17:13:47 +0800

    Changed timeline screen to use sliver list and sliver app bar.

--- lib/screens/timeline_screen.dart
+++ lib/screens/timeline_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sgcovidmapper/models/timeline/date_timeline_item.dart';
-import 'package:sgcovidmapper/models/timeline/divider_timeline_item.dart';
-import 'package:sgcovidmapper/models/timeline/location_timeline_item.dart';
 import 'package:sgcovidmapper/models/timeline/timeline.dart';
 import 'package:sgcovidmapper/models/timeline/timeline_item.dart';
 import 'package:sgcovidmapper/models/timeline/timeline_model.dart';
@@ -17,39 +14,57 @@ class TimelineScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(),
-        body: ListView.builder(
-          itemCount: timelineModel.tiles.length,
-          itemBuilder: (context, index) {
-            TimelineItem model = timelineModel.tiles[index];
-
-            if (model is DividerTimelineItem) {
-              return model.divider;
-            } else if (model is TimelineIndicator) {
-              return TimelineTile(
-                alignment: TimelineAlign.manual,
-                isFirst: index == 0,
-                isLast: index == timelineModel.tiles.length - 1,
-                bottomLineStyle: LineStyle(color: Colors.blueGrey),
-                topLineStyle: LineStyle(color: Colors.blueGrey),
-                lineX: model.lineX,
-                hasIndicator: true,
-                indicatorStyle: IndicatorStyle(
-                    padding: EdgeInsets.all(6.0),
-                    indicator: (model as TimelineIndicator).indicator,
-                    width: model is DateTimelineItem ? 80 : 50,
-                    height: 50),
-                rightChild: model is LocationTimelineItem ? model.child : null,
-                leftChild: model is VisitTimelineItem ? model.child : null,
-              );
-            } else {
-              return Container(
-                width: 0,
-                height: 0,
-              );
-            }
+        body: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (notification) {
+            notification.disallowGlow();
+            return false;
           },
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                title: Text('Timeline'),
+                floating: true,
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  // The builder function returns a ListTile with a title that
+                  // displays the index of the current item.
+                  (context, index) {
+                    TimelineItem model = timelineModel.tiles[index];
+
+                    if (model is DividerTimelineItem) {
+                      return model.divider;
+                    } else if (model is TimelineIndicator) {
+                      return TimelineTile(
+                        alignment: TimelineAlign.manual,
+                        isFirst: index == 0,
+                        isLast: index == timelineModel.tiles.length - 1,
+                        bottomLineStyle: LineStyle(color: Colors.blueGrey),
+                        topLineStyle: LineStyle(color: Colors.blueGrey),
+                        lineX: model.lineX,
+                        hasIndicator: true,
+                        indicatorStyle: IndicatorStyle(
+                            padding: EdgeInsets.all(6.0),
+                            indicator: (model as TimelineIndicator).indicator,
+                            width: model is DateTimelineItem ? 80 : 50,
+                            height: 50),
+                        rightChild:
+                            model is LocationTimelineItem ? model.child : null,
+                        leftChild:
+                            model is VisitTimelineItem ? model.child : null,
+                      );
+                    } else {
+                      return Container(
+                        width: 0,
+                        height: 0,
+                      );
+                    }
+                  },
+                  childCount: timelineModel.tiles.length,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
